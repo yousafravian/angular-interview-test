08aba8a0d45581faa5f2de10c8388409d3d5d681
Yousaf Raza <yousafraza@hardstoneenterprises.com>
2022-11-15 22:05:16 +0500

    ui-task added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-
-import { SurveyList } from './surveyList/surveyList.component';
-import { Filters } from './filters/filters.component';
 import { RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SurveyList,
-    Filters
   ],
   imports: [
     BrowserModule,
-    RouterTestingModule,
-    RouterModule.forRoot([
-      { path:'', component: SurveyList }
-    ])
+    RouterTestingModule
   ],
   exports: [
     RouterModule
